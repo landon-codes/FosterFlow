4287efa5a38ddd873f8cb0afbccf3cd00e84ee94
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-04 16:09:52 -0700

    Remove the Position property from the Sprite class. This seems to cause issues when creating classes that use the sprite class, and it should never be used alone.

--- Graphics/Sprite.cs
+++ Graphics/Sprite.cs
@@ -3,15 +3,14 @@ using System.Numerics;
 
 namespace FosterFlow.Graphics;
 
-public class Sprite(Subtexture spriteTexture, Vector2 spritePosition, float spriteScale)
+public class Sprite(Subtexture spriteTexture, float spriteScale)
 {
     public Subtexture Texture = spriteTexture;
-    public Vector2 Position = spritePosition;
     public float Scale = spriteScale;
     public float Rotation = 0.0f;
 
-    public void Draw(Batcher batcher)
+    public void Draw(Batcher batcher, Vector2 position)
     {
-        batcher.Image(Texture, Position, new Vector2(0, 0), new Vector2(Scale), Rotation, Color.White);
+        batcher.Image(Texture, position, new Vector2(0, 0), new Vector2(Scale), Rotation, Color.White);
     }
 }
