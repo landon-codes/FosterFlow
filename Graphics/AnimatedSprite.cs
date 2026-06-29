ab60b8097e99c1ba1bcd5b5f82ccfceb04fc6121
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-29 10:35:10 -0500

    Refactor AnimatedSprite.cs to use the new Animation struct

--- Graphics/AnimatedSprite.cs
+++ Graphics/AnimatedSprite.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using Foster.Framework;
 
 namespace BaobabEngine.Graphics;
 
 public class AnimatedSprite: Sprite
 {
     // Contains the different animations for a sprite organized by name
-    private Dictionary<string, List<Subtexture>> _animations;
+    private readonly Dictionary<string, Animation> _animations;
     
     // The current animation that is being played from the dictionary ↑
     private string _currentAnimation;
@@ -23,18 +22,16 @@ public class AnimatedSprite: Sprite
     /// </summary>
     /// <param name="spriteAnimations">The dictionary containing the different animations for your sprite</param>
     /// <param name="startingAnimation">The key to the animation your sprite will start using from the dictionary</param>
-    /// <param name="animationDelay">The floating point value delay in seconds between animation frames</param>
     /// <param name="spriteScale">The scale at which your sprite will be drawn</param>
     /// <param name="spriteRotation">The initial rotation your sprite will have.</param>
-    public AnimatedSprite(Dictionary<string, List<Subtexture>> spriteAnimations,
+    public AnimatedSprite(Dictionary<string, Animation> spriteAnimations,
         string startingAnimation,
-        float animationDelay,
         float spriteScale = 1,
         float spriteRotation = 0)
     {
         _animations = spriteAnimations;
         _currentAnimation = startingAnimation;
-        _animationDelay = animationDelay;
+        _animationDelay = spriteAnimations[startingAnimation].Delay;
         _elapsedTime = 0;
         
         Scale = spriteScale;
@@ -47,7 +44,9 @@ public class AnimatedSprite: Sprite
         if (_currentAnimation == animationName && !restartIfNotChanged)
             return;
             
+        // Update animation data
         _currentAnimation = animationName;
+        _animationDelay = _animations[_currentAnimation].Delay;
         
         // Resets the frame so it will start at the first frame.
         _currentFrame = 0;
@@ -57,20 +56,18 @@ public class AnimatedSprite: Sprite
     // Set's the texture to the current frame in the current animation
     private void UpdateTexture()
     {
-        Texture = _animations[_currentAnimation][_currentFrame];
+        Texture = _animations[_currentAnimation].Frames[_currentFrame];
     }
 
     public void Update(float deltaTime)
     {
         _elapsedTime += deltaTime;
-
+        if (!(_elapsedTime >= _animationDelay)) return;
+            
         // Updates the frame and time values
-        if (_elapsedTime >= _animationDelay)
-        {
-            _elapsedTime -= _animationDelay;
-            _currentFrame = (_currentFrame + 1) % _animations[_currentAnimation].Count;
+        _elapsedTime -= _animationDelay;
+        _currentFrame = (_currentFrame + 1) % _animations[_currentAnimation].Frames.Length;
 
-            UpdateTexture();
-        }
+        UpdateTexture();
     }
 }
