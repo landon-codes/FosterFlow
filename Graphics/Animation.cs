2cedd9aef90015f8d1fabb46dbb7fba0edb9fe72
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-29 10:34:51 -0500

    Create an Animation struct

---
+++ Graphics/Animation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Foster.Framework;
+
+namespace BaobabEngine.Graphics;
+
+public struct Animation(Subtexture[] animationFrames, float delayBetweenFrames)
+{
+    public Subtexture[] Frames { get; private set; } = animationFrames;
+    
+    // In seconds
+    public float Delay { get; private set; } = delayBetweenFrames;
+}
