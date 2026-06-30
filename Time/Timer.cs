51d399ef3663444488718a1274c68d6b334bd465
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-30 07:53:00 -0500

    Create Time/Timer.cs

---
+++ Time/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BaobabEngine.Time;
+
+public class Timer(float durationInSeconds)
+{
+    // Subscribe functions to be run once complete
+    public event EventHandler OnComplete;
+    
+    private float _elapsedTime;
+    
+    public void Update(float deltaTime, object sender, EventArgs args)
+    {
+        _elapsedTime += deltaTime;
+
+        // Invokes subscribed functions and resets the timer once complete
+        if (_elapsedTime >= durationInSeconds)
+        {
+            _elapsedTime -= durationInSeconds;
+            OnComplete?.Invoke(sender, args);
+        }
+    }
+}
