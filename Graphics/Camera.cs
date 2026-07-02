4c9868ef5bac5e8c3f7157964ff4a528980f1af6
Landon <230772997+landon-codes@users.noreply.github.com>
2026-07-02 09:50:24 -0500

    Create Graphics/Camera.cs
    * Create a camera class
    I'm still working on figuring out how everything actually works, but this is a great starting point!
    
    * Update Camera.cs
    Cached the starting position value so it could be used under the reset method

---
+++ Graphics/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Foster.Framework;
+
+namespace BaobabEngine.Graphics;
+
+public class Camera(Vector2 origin, Vector2 startingPosition, bool relative = false)
+{
+    private readonly Vector2 _startingPosition = startingPosition;
+    
+    public Vector2 Position = startingPosition;
+    public float Zoom = 1.0f;
+    public float Rotation;
+
+    // Pushes the camera to the batcher to be applied when rendering
+    public void Apply(in Batcher batcher)
+    {
+        batcher.PushMatrix(Position, origin, new Vector2(Zoom), Rotation, relative);
+    }
+
+    public void Reset()
+    {
+        Position = _startingPosition;
+        Zoom = 1.0f;
+        Rotation = 0.0f;
+    }
+}
