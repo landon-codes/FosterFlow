e2cbf6386b1c8322d6a30f11a28f123040b97c88
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-07-02 09:37:33 -0500

    Create a camera class I'm still working on figuring out how everything actually works, but this is a great starting point!

---
+++ Graphics/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using Foster.Framework;
+
+namespace BaobabEngine.Graphics;
+
+public class Camera(Vector2 origin, Vector2 startingPosition, bool relative = false)
+{
+    public Vector2 Position = startingPosition;
+    public float Zoom = 1.0f;
+    public float Rotation = 0.0f;
+
+    // Pushes the camera to the batcher to be applied when rendering
+    public void Apply(in Batcher batcher)
+    {
+        batcher.PushMatrix(Position, origin, new Vector2(Zoom), Rotation, relative);
+    }
+
+    public void Reset()
+    {
+        Position = startingPosition;
+        Zoom = 1.0f;
+        Rotation = 0.0f;
+    }
+}
